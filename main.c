ad8a11d89679b3f285b28702970de0f85bec384d
MandarMG <mandarmgole@gmail.com>
2015-11-02 20:20:18 +0100

    Verified usage

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 int main()
 {
     // Set bit 4
-    DDRC=0x10;
+    DDRC=(1 << PC4);
     while (1)
     {
         // Invert the bit 4 of PORTC
-        PORTC ^= 0x10;
+        PORTC ^= (1 << PC4);
 
         // Wait 500ms (the leb connectod to the bit 7 of PORTC will blink at 1Hz)
-        _delay_ms(50);
+        _delay_ms(500);
     }
 }
